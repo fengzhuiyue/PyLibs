418d2a6ba5d9aed80b0518a76a69d180d426eefd
李阿福茶铺 <ahlforslee@gmail.com>
2013-11-15 22:46:43 +0800

    Update douban.py

--- socket/douban.py
+++ socket/douban.py
@@ -1,3 +1,4 @@
+from util import *
 import re,urllib,cookielib,urllib2
 import socket
 socket.setdefaulttimeout(5)
@@ -65,3 +66,42 @@ class douban(object):
             print page.read()
             return 0
         return 1
+dou=douban()
+username=raw_input()
+password=raw_input()
+domain='http://www.douban.com/'
+origURL='http://www.douban.com/login'
+dou.setinfo(username,password,domain,origURL)
+dou.signin()
+page=dou.opener.open('http://www.douban.com/contacts/list')
+directions=re.findall(r'\s*href\s*=\s*"http://www.douban.com/people/([^/]*)',page.read())
+dir={}
+dir_book={}
+i=0
+for direction in directions:
+    dir[direction]=0
+num=len(dir)
+i=0
+for nam, nothing in dir.items():
+    name="http://book.douban.com/list/"+nam+"/collect"
+    print name
+    #i=i+1
+    print "books of",nam,":"
+    page=dou.opener.open(name)
+    while True:
+        p=page.read()
+        books=re.findall(r'href="http://book.douban.com/subject/.*/">\s*([^<]*)',p)
+        for book in books:
+            print book
+            if book in dir_book:
+                dir_book[book]=dir_book[book]+1
+            else:
+                dir_book[book]=1
+        ds=re.search(r'</a><span class="next"><a[\n\s]*href="(http://[^"]*)">',p)      
+        if ds == None:
+            break
+        page=dou.opener.open(ds.group(1))     
+        
+for book_name,times in sortDic(dir_book):
+    print book_name+": "+str(times)
+print len(dir_book) 
